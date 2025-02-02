f0c92bfd93ec9384fa08e6e0a5df49f121b857d5
Abel Borit <abelboritguitton@gmail.com>
2025-02-02 11:48:58 -0500

    [add] images & gif

---
+++ src/assets/gif/index.ts
@@ -0,0 +1,19 @@
+import pregunta_inicial from "./pregunta_inicial.gif";
+import respuesta_si from "./respuesta_si.gif";
+import respuesta_no_1 from "./respuesta_no_1.gif";
+import respuesta_no_2 from "./respuesta_no_2.gif";
+import respuesta_no_3 from "./respuesta_no_3.gif";
+import respuesta_no_4 from "./respuesta_no_4.gif";
+import respuesta_no_5 from "./respuesta_no_5.gif";
+import respuesta_no_6 from "./respuesta_no_6.gif";
+
+export {
+  pregunta_inicial,
+  respuesta_si,
+  respuesta_no_1,
+  respuesta_no_2,
+  respuesta_no_3,
+  respuesta_no_4,
+  respuesta_no_5,
+  respuesta_no_6,
+};
