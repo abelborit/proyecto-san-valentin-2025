09a6925e4dc812ebad860d41b22cd39e018d9466
Abel Borit <abelboritguitton@gmail.com>
2025-02-06 21:23:27 -0500

    [update] defineConfig to add path base

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "./", // especificar la ruta base para la aplicación para usar rutas relativas
 })
