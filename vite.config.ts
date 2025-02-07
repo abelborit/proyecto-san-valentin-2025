580e382bad30092c8ec8150e0e02fb8f95a626e9
Abel Borit <abelboritguitton@gmail.com>
2025-02-06 22:06:42 -0500

    [update] deploy config and package.json

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/proyecto-san-valentin-2025/", // especificar la ruta base para la aplicación para usar rutas relativas
+  base: "https://abelborit.github.io/proyecto-san-valentin-2025/",
 })
