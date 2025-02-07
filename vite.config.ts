b242f8e4e21361d227723bd1bb619d55680f55f6
Abel Borit <abelboritguitton@gmail.com>
2025-02-06 21:44:40 -0500

    [update] defineConfig to update path base

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./", // especificar la ruta base para la aplicación para usar rutas relativas
+  base: "/proyecto-san-valentin-2025/", // especificar la ruta base para la aplicación para usar rutas relativas
 })
